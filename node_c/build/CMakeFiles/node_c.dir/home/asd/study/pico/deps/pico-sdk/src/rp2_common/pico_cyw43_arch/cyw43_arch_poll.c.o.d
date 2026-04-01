de4ef4277f51dca6ef2779ab846b2cd499dbdb6c
Gwiin <injoa4526@gmail.com>
2026-04-01 12:07:42 +0900

    feat: add node_c console workflow to main

---
+++ node_c/build/CMakeFiles/node_c.dir/home/asd/study/pico/deps/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/node_c.dir/home/asd/study/pico/deps/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c.o: \
+ /home/asd/study/pico/deps/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c
---
+++ node_c/build/_deps/picotool-build/data_locs.cpp
@@ -0,0 +1,7 @@
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> data_locs = {
+    "./","/home/asd/hrd_first_project/node_c/build/_deps/picotool/"
+};
